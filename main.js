c36edf52387523292691fa964de10735e4be1253
Zenic1 <76486597+Zenic1@users.noreply.github.com>
2024-06-01 19:55:29 +0200

    change folder structure

---
+++ main.js
@@ -0,0 +1,22 @@
+const menu = document.getElementById('menu');
+const menuToggle = document.getElementById('burger');
+const black = document.getElementsByClassName('black-screen')[0]
+const body = document.body
+
+
+// Funkcja otwierania/zamykania menu
+function toggleMenu() {
+
+    if (!black.classList.contains('menu-open')) {
+        menu.style.left = 0;
+        body.style.overflow = 'hidden';
+        black.classList.add('menu-open');
+    } else{
+        menu.style.left = '-100%';
+        body.style.overflow = 'auto';
+        black.classList.remove('menu-open');
+    }
+}
+menuToggle.addEventListener('click', toggleMenu);
+black.addEventListener('click', toggleMenu);
+
